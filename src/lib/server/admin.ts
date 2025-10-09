4ded012b0cb649d0107a33574079bf834afde5d4
Mihaela Dumitrache <mihaeladumitrache@Mihaelas-MacBook-Pro.local>
2025-10-09 16:11:17 +0200

    PWA settings

--- src/lib/server/admin.ts
+++ src/lib/server/admin.ts
@@ -6,9 +6,8 @@ import {
 } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
-
+const firebaseEnv = env.FIREBASE_ADMIN_KEY;
 const serviceAccount = JSON.parse(env.FIREBASE_ADMIN_KEY);
-
 let defaultApp;
 if (!getApps().length) {
 
